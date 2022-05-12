e9a779adb3bd14530457e25b8b6853f1f743dbae
Andrew Krysh <vip.krysh@mail.ru>
2022-05-12 15:13:21 +0500

    вёрстка страницы с информацией о фильме

---
+++ app/src/main/java/studio/stilip/tensorkinopoisk/ui/films/FilmInfo/FilmInfoActivity.kt
@@ -0,0 +1,12 @@
+package studio.stilip.tensorkinopoisk.ui.films.FilmInfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import studio.stilip.tensorkinopoisk.R
+
+class FilmInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_info)
+    }
+}
