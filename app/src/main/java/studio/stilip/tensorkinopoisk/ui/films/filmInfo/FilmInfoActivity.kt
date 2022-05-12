b0ef6d9278aaea914bcea069be8506c013acbb1f
Andrew Krysh <vip.krysh@mail.ru>
2022-05-12 15:39:45 +0500

    кнопка назад и кампоньен на вызов данной страницы

--- app/src/main/java/studio/stilip/tensorkinopoisk/ui/films/filmInfo/FilmInfoActivity.kt
+++ app/src/main/java/studio/stilip/tensorkinopoisk/ui/films/filmInfo/FilmInfoActivity.kt
@@ -1,12 +1,44 @@
 package studio.stilip.tensorkinopoisk.ui.films.filmInfo
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import studio.stilip.tensorkinopoisk.R
+import studio.stilip.tensorkinopoisk.databinding.ActivityFilmInfoBinding
 
 class FilmInfoActivity : AppCompatActivity() {
+
+    private val binding: ActivityFilmInfoBinding by lazy {
+        ActivityFilmInfoBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_film_info)
+        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+
+        private const val EXTRA_FILM_ID = "extra_film_id"
+
+        fun createIntent(context: Context, filmId: String): Intent {
+            return Intent(context, FilmInfoActivity::class.java).apply {
+                putExtra(EXTRA_FILM_ID, filmId)
+            }
+        }
     }
 }
